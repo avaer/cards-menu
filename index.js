ef8c225d1bd8e73626dee2a65e4782b67b880f7c
Avaer Kazmer <a@modules.io>
2021-09-10 16:27:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,259 @@
+import * as THREE from 'three';
+import metaversefile from 'metaversefile';
+const {useApp, useFrame, useInternals} = metaversefile;
+
+const cardPreviewHost = `https://card-preview.exokit.org`;
+
+const cardWidth = 0.063;
+const cardHeight = cardWidth / 2.5 * 3.5;
+const cardsBufferFactor = 1.1;
+const menuWidth = cardWidth * cardsBufferFactor * 4;
+const menuHeight = cardHeight * cardsBufferFactor * 4;
+const menuRadius = 0.0025;
+const _loadImage = u => new Promise((accept, reject) => {
+  const img = new Image();
+  img.crossOrigin = 'Anonymous';
+  
+  img.src = u;
+  img.onload = () => {
+    accept(img);
+  };
+  img.onerror = reject;
+});
+function makeShape(shape, x, y, width, height, radius) {
+  shape.absarc( x - width/2, y + height/2, radius, Math.PI, Math.PI / 2, true );
+  shape.absarc( x + width/2, y + height/2, radius, Math.PI / 2, 0, true );
+  shape.absarc( x + width/2, y - height/2, radius, 0, -Math.PI / 2, true );
+  shape.absarc( x - width/2, y - height/2, radius, -Math.PI / 2, -Math.PI, true );
+  return shape;
+}
+function createBoxWithRoundedEdges( width, height, radius, innerFactor) {
+  const shape = makeShape(new THREE.Shape(), 0, 0, width, height, radius);
+  const hole = makeShape(new THREE.Path(), 0, 0, width * innerFactor, height * innerFactor, radius);
+  shape.holes.push(hole);
+
+  const geometry = new THREE.ShapeGeometry(shape);
+  return geometry;
+}
+const cardFrontGeometry = new THREE.PlaneBufferGeometry(cardWidth, cardHeight);
+const cardBackGeometry = cardFrontGeometry.clone()
+  .applyMatrix4(
+    new THREE.Matrix4().compose(
+      new THREE.Vector3(0, 0, -0.001),
+      new THREE.Quaternion()
+        .setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI),
+      new THREE.Vector3(1, 1, 1)
+    )
+  );
+const _makeCardBackMaterial = () => {
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    map: new THREE.Texture(),
+    transparent: true,
+  });
+  (async () => {
+    const img = await _loadImage('./assets/cardback.png');
+    material.map.image = img;
+    material.map.minFilter = THREE.LinearMipmapLinearFilter;
+    material.map.magFilter = THREE.LinearFilter;
+    material.map.encoding = THREE.sRGBEncoding;
+    material.map.anisotropy = 16;
+    material.map.needsUpdate = true;
+  })();
+  return material;
+};
+const _makeCardMesh = img => {
+  const geometry = cardFrontGeometry;
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xFFFFFF,
+    map: new THREE.Texture(img),
+    side: THREE.DoubleSide,
+    transparent: true,
+  });
+  material.map.minFilter = THREE.LinearMipmapLinearFilter;
+  material.map.magFilter = THREE.LinearFilter;
+  material.map.encoding = THREE.sRGBEncoding;
+  material.map.anisotropy = 16;
+  material.map.needsUpdate = true;
+  const mesh = new THREE.Mesh(geometry, material);
+  
+  {
+    const geometry = cardBackGeometry;
+    const material = _makeCardBackMaterial();
+    const back = new THREE.Mesh(geometry, material);
+    mesh.add(back);
+    mesh.back = back;
+  }
+  
+  return mesh;
+};
+const _makeInventoryMesh = () => {
+  const w = menuWidth + menuRadius*2;
+  const h = menuHeight + menuRadius*2;
+  const geometry = createBoxWithRoundedEdges(w, h, menuRadius, 0.99);
+  const boundingBox = new THREE.Box3().setFromObject(new THREE.Mesh(geometry));
+  // console.log('got bounding box', boundingBox);
+  const material = new THREE.ShaderMaterial({
+    uniforms: {
+      uBoundingBox: {
+        type: 'vec4',
+        value: new THREE.Vector4(
+          boundingBox.min.x,
+          boundingBox.min.y,
+          boundingBox.max.x - boundingBox.min.x,
+          boundingBox.max.y - boundingBox.min.y
+        ),
+        needsUpdate: true,
+      },
+      uTime: {
+        type: 'f',
+        value: 0,
+        needsUpdate: true,
+      },
+      uTimeCubic: {
+        type: 'f',
+        value: 0,
+        needsUpdate: true,
+      },
+    },
+    vertexShader: `\
+      precision highp float;
+      precision highp int;
+
+      // uniform float uTime;
+      uniform vec4 uBoundingBox;
+      varying vec3 vPosition;
+      // varying vec3 vNormal;
+
+      void main() {
+        vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+        gl_Position = projectionMatrix * mvPosition;
+        
+        vPosition = (
+          position - vec3(uBoundingBox.x, uBoundingBox.y, 0.)
+        ) / vec3(uBoundingBox.z, uBoundingBox.w, 1.);
+        // vNormal = normal;
+      }
+    `,
+    fragmentShader: `\
+      precision highp float;
+      precision highp int;
+
+      #define PI 3.1415926535897932384626433832795
+
+      // uniform vec4 uBoundingBox;
+      uniform float uTime;
+      uniform float uTimeCubic;
+      varying vec3 vPosition;
+      // varying vec3 vNormal;
+
+      vec3 hueShift( vec3 color, float hueAdjust ){
+        const vec3  kRGBToYPrime = vec3 (0.299, 0.587, 0.114);
+        const vec3  kRGBToI      = vec3 (0.596, -0.275, -0.321);
+        const vec3  kRGBToQ      = vec3 (0.212, -0.523, 0.311);
+
+        const vec3  kYIQToR     = vec3 (1.0, 0.956, 0.621);
+        const vec3  kYIQToG     = vec3 (1.0, -0.272, -0.647);
+        const vec3  kYIQToB     = vec3 (1.0, -1.107, 1.704);
+
+        float   YPrime  = dot (color, kRGBToYPrime);
+        float   I       = dot (color, kRGBToI);
+        float   Q       = dot (color, kRGBToQ);
+        float   hue     = atan (Q, I);
+        float   chroma  = sqrt (I * I + Q * Q);
+
+        hue += hueAdjust;
+
+        Q = chroma * sin (hue);
+        I = chroma * cos (hue);
+
+        vec3    yIQ   = vec3 (YPrime, I, Q);
+
+        return vec3( dot (yIQ, kYIQToR), dot (yIQ, kYIQToG), dot (yIQ, kYIQToB) );
+    }
+
+      void main() {
+        if (
+          (1. - vPosition.y) < uTimeCubic &&
+          abs(vPosition.x - 0.5) < uTimeCubic
+        ) {
+          gl_FragColor = vec4(hueShift(vPosition, uTime * PI * 2.), 1.);
+        } else {
+          discard;
+        }
+      }
+    `,
+    // transparent: true,
+    // polygonOffset: true,
+    // polygonOffsetFactor: -1,
+    // polygonOffsetUnits: 1,
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  
+  const cards = [];
+  (async () => {
+    const cardImgs = [];
+    const numCols = 4;
+    const numRows = 6;
+    const promises = [];
+    let i = 0;
+    for (let row = 0; row < numRows; row++) {
+      for (let col = 0; col < numCols; col++) {
+        const promise = (async () => {
+          const index = i;
+          const id = ++i;
+          const w = 1024;
+          const ext = 'png';
+          const u = `${cardPreviewHost}/?t=${id}&w=${w}&ext=${ext}`;
+          const img = await _loadImage(u);
+          
+          const cardMesh = _makeCardMesh(img);
+          cardMesh.basePosition = new THREE.Vector3(
+            menuRadius - menuWidth/2 + cardWidth/2 + col * cardWidth * cardsBufferFactor,
+            -menuRadius + menuHeight/2 - cardHeight/2 - row * cardHeight * cardsBufferFactor,
+            0
+          );
+          cardMesh.position.copy(cardMesh.basePosition);
+          cardMesh.index = index;
+          mesh.add(cardMesh);
+          cards.push(cardMesh);
+        })();
+        promises.push(promise);
+      }
+    }
+    await Promise.all(promises);
+  })();
+  
+  useFrame(() => {
+    const maxTime = 2000;
+    const f = (Date.now() % maxTime) / maxTime;
+    
+    material.uniforms.uTime.value = f;
+    material.uniforms.uTime.needsUpdate = true;
+    material.uniforms.uTimeCubic.value = cubicBezier(f);
+    material.uniforms.uTimeCubic.needsUpdate = true;
+    
+    // window.cards = cards;
+    for (const card of cards) {
+      const {index} = card;
+      const cardStartTime = index * 0.02;
+      const g = cubicBezier(f - cardStartTime);
+      const h = 1 - g;
+      card.position.copy(card.basePosition)
+        .lerp(
+          card.basePosition.clone()
+            .add(new THREE.Vector3(0, -0.1, 0))
+          ,
+          h
+        );
+      card.material.opacity = g;
+      card.back.material.opacity = g;
+    }
+  });
+  
+  return mesh;
+};
+
+export default () => {
+  
+};
